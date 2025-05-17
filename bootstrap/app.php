3e10c730c7b166e2e99e7ce0daa7f729df8bbbe3
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2025-05-18 01:27:10 +0300

    disabled CSRF verification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(EnsureFrontendRequestsAreStateful::class);
+        $middleware->validateCsrfTokens(except: ['*']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
