f180a96a2b826a26641dba7647a411516df12ee9
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2025-05-18 01:01:25 +0300

    update routes/web.php, delete database/.gitignore

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiTokenController;
 use App\Http\Controllers\CarController;
+use Laravel\Sanctum\PersonalAccessToken;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,7 +14,8 @@ Route::get('/', function () {
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/api/tokens/create', [ApiTokenController::class, 'createToken']);
     Route::get('/api/user', function (Request $request) {
-        return $request->user();
+        $token = PersonalAccessToken::findToken($request->bearerToken());
+        return $token->tokenable;
     });
     Route::apiResource('/cars', CarController::class);
 });
